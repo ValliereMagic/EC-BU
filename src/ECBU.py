9959f50ff8d36707b898ee0e0392935dd06ad6a1
ValliereMagic <ValliereMagic@users.noreply.github.com>
2020-05-15 23:19:53 -0400

    Initial Upload

---
+++ src/ECBU.py
@@ -0,0 +1,222 @@
+import pickle
+import os.path
+import time
+import math
+import hashlib
+from ECBUMediaUpload import ECBUMediaUpload
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+
+SCOPES = ["https://www.googleapis.com/auth/drive.file"]
+
+
+def get_credentials():
+    credentials = None
+    # Check if we have saved credentials
+    if os.path.exists('token.pickle'):
+        with open('token.pickle', 'rb') as token:
+            credentials = pickle.load(token)
+    # Request credentials
+    if not credentials or not credentials.valid:
+        # Credentials require refreshing
+        if credentials and credentials.expired and credentials.refresh_token:
+            credentials.refresh(Request())
+        # Need to request credentials
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            credentials = flow.run_local_server(port=0)
+            # Save the credentials to the token file
+            with open('token.pickle', 'wb') as token:
+                pickle.dump(credentials, token)
+    return credentials
+
+
+def find_or_create_backup_folder(service, dest_folder_name: str) -> str:
+    # Find the upload folder
+    folder_id = None
+    page_token = None
+    while True:
+        response = service.files().list(q="mimeType = 'application/vnd.google-apps.folder' and trashed = false",
+                                        spaces='drive', fields='nextPageToken, files(id, name)',
+                                        pageToken=page_token).execute()
+        # Look through each of the files accessable to this application, and see if there is already a folder
+        # there for backing up this file.
+        for file in response.get('files', []):
+            folder_name: str = file.get('name')
+            file_id: str = file.get('id')
+            # Found a folder to upload this file to
+            # already.
+            if folder_name == dest_folder_name:
+                folder_id = file_id
+        page_token = response.get('nextPageToken', None)
+        if page_token is None:
+            break
+    # Folder doesn't exist, we need to create one
+    if folder_id is None:
+        result = service.files().create(body={
+            'name': dest_folder_name,
+            'mimeType': 'application/vnd.google-apps.folder'
+        }, fields='id').execute()
+        folder_id = result.get('id')
+    return folder_id
+
+
+def hash_ecbu_media_file_upload(file_chunk: ECBUMediaUpload) -> str:
+    hasher = hashlib.md5()
+    # Append each chunk of the file to the hasher
+    bytes_hashed: int = 0
+    while bytes_hashed < file_chunk.size():
+        current_chunk: bytes = file_chunk.getbytes(
+            bytes_hashed, file_chunk.chunksize())
+        bytes_hashed += len(current_chunk)
+        hasher.update(current_chunk)
+    # Turn it into a hex_digest and return
+    return hasher.hexdigest()
+
+
+# Simple struct returned by check_if_chunk_exists to detect chunk changes
+# (Which need to be uploaded and which don't)
+class ChangedFile:
+    def __init__(self, changed: bool, ident: str, md5hash: str = None):
+        self.changed = changed
+        self.ident = ident
+        self.md5hash = md5hash
+
+
+def check_if_chunk_exists_or_changed(service, file_chunk: ECBUMediaUpload, folder_id: str, file_chunk_name: str) -> ChangedFile:
+    page_token = None
+    while True:
+        response = service.files().list(q="mimeType = 'application/octet-stream' and '" + folder_id + "' in parents",
+                                        spaces='drive', fields='nextPageToken, files(id, name, md5Checksum)', pageToken=page_token).execute()
+        for file in response.get('files', []):
+            file_name: str = file.get('name')
+            # Check if this is a match
+            if file_chunk_name == file_name:
+                file_id: str = file.get('id')
+                md5hash: str = file.get('md5Checksum')
+                local_hash: str = hash_ecbu_media_file_upload(file_chunk)
+                # Check whether this chunk has changed since last time
+                # it was uploaded by comparing the hashes.
+                if md5hash == local_hash:
+                    return ChangedFile(False, file_id)
+                else:
+                    return ChangedFile(True, file_id, local_hash)
+        # Move on to the next page
+        page_token = response.get('nextPageToken', None)
+        if page_token is None:
+            break
+    # The file was not found, hash it and return
+    return ChangedFile(True, None, hash_ecbu_media_file_upload(file_chunk))
+
+
+def backup_chunked_file_piece(service, file_chunk: ECBUMediaUpload, folder_id: str, file_chunk_name: str):
+    print("Beginning upload of chunk: " + file_chunk_name + ".")
+    # Check whether this chunk has been uploaded before
+    file_status: ChangedFile = check_if_chunk_exists_or_changed(
+        service, file_chunk, folder_id, file_chunk_name)
+    # Upload the file_chunk to google drive
+    request = None
+    # Chunk has never been uploaded before
+    if file_status.changed and not file_status.ident:
+        request = service.files().create(
+            media_body=file_chunk, body={'name': file_chunk_name, 'parents': [folder_id]})
+    # Chunk has been uploaded before but it has been changed
+    elif file_status.changed and file_status.ident:
+        request = service.files().update(
+            media_body=file_chunk, fileId=file_status.ident)
+    # The chunk has not changed and does not need to be re-uploaded.
+    else:
+        print("Chunk: " + file_chunk_name + " is already up to date!")
+        return
+    # beginning back-off duration for if an error occurs and we try to resume
+    default_backoff_time: float = 0.5
+    backoff_time: float = default_backoff_time
+    response = None
+    while response is None:
+        # Attempt to upload a chunk of the file
+        try:
+            status, response = request.next_chunk()
+        # Catch any errors that occur, attempting to continue
+        # uploading if possible
+        except HttpError as e:
+            if e.resp.status in [404]:
+                # Restart upload
+                print("Error 404. Must restart upload.")
+                return False
+            elif e.resp.status in [500, 502, 503, 504]:
+                # Call next chunk again, using exponential backoff
+                print("An error occurred. Trying again with exponential backoff. Waiting: " +
+                      str(backoff_time) + " seconds.")
+                time.sleep(backoff_time)
+                backoff_time *= 2
+                response = None
+                continue
+            else:
+                # Error and quit
+                print("Fatal Error uploading chunk.")
+                return False
+        # Reset exponential backoff time amount
+        backoff_time = default_backoff_time
+        if status:
+            print("Chunk upload progress: %d%%." %
+                  int(status.progress() * 100))
+    print("Upload of Chunk: " + file_chunk_name + " Complete!")
+
+
+def begin_backup(service, local_file_name: str, dest_folder_name: str, file_chunk_size: int) -> bool:
+    """
+    service: google drive service
+    local_file_name: name of the file on disk
+    dest_folder_name: the name of the folder for this to be stored in on google drive
+    file_chunk_size: the size in MB for each of the chunks in the uploaded folder to be.
+    """
+    # Get or create the parent folder for our chunked backup file
+    folder_id: str = find_or_create_backup_folder(service, dest_folder_name)
+    # Unable to find or make a folder to back up the file to
+    if folder_id is None:
+        return False
+    # Open up the file and start chunking
+    with open(local_file_name, 'rb') as test_file:
+        # Calculate the size of the file to backup
+        test_file.seek(os.SEEK_SET)
+        test_file.seek(0, os.SEEK_END)
+        file_size: int = test_file.tell()
+        # Calculate the number of file_chunk_size chunks to separate and upload
+        file_chunk_size_MB = (file_chunk_size * (1000 * 1000))
+        num_chunk_files: int = math.ceil(file_size / file_chunk_size_MB)
+        # Create a Media upload for each of the separated files and upload each of them
+        bytes_uploaded: int = 0
+        for chunk_num in range(1, num_chunk_files + 1):
+            # Find the end index for the current file chunk
+            end_index: int = bytes_uploaded + file_chunk_size_MB
+            # If this is the last chunk, and it goes out of bounds,
+            # shorten it so that it doesn't
+            if end_index >= file_size:
+                end_index = file_size - 1
+            # Create the ECBUMediaUpload object to represent this chunk of the file
+            file_chunk = ECBUMediaUpload(
+                test_file, file_size, bytes_uploaded, end_index)
+            # Upload this chunk to google drive
+            backup_chunked_file_piece(
+                service, file_chunk, folder_id, dest_folder_name + '.' + str(chunk_num))
+            bytes_uploaded += file_chunk_size_MB
+        return True
+
+
+def main():
+    credentials = get_credentials()
+    # Check if None
+    if credentials is None:
+        print("Unable to acquire credentials")
+        return
+    # Create the drive service
+    service = build('drive', 'v3', credentials=credentials)
+    begin_backup(
+        service, 'test.txt', 'DisInf', 5)
+
+
+if __name__ == '__main__':
+    main()
---
+++ src/ECBUMediaUpload.py
@@ -0,0 +1,84 @@
+from io import BufferedReader
+import os
+from googleapiclient.http import MediaUpload
+
+
+class InvalidChunkException(ValueError):
+    pass
+
+
+class OffsetOutOfBoundsException(ValueError):
+    pass
+
+
+class ECBUMediaUpload(MediaUpload):
+    def __init__(self, read_file: BufferedReader, file_size: int, begin_index: int, end_index: int,
+                 chunk_size: int = 1 * (1024 * 1024), resumable: bool = True):
+        """
+        This class represents a piece of a file to be uploaded as a file.
+        It wraps a BufferedReader for a whole file, but only allows it to read
+        starting at begin_offset until it reaches end_offset.
+
+        read_file: file descriptor, returned from an open statement.
+        file_size: the length of the file in bytes.
+        begin_index: the beginning index of the file that makes up this upload.
+        end_index: the end index of the file that makes up this upload.
+        chunk size: the amount of the file pulled into memory and uploaded at a time.
+        resumable: whether this upload uses the resumable feature of
+            google drive. We want this.
+        """
+        self._read_file = read_file
+        self._mimetype = "application/octet-stream"
+        # check if we are within the bounds of the file
+        if (begin_index > (file_size - 1) or end_index > (file_size - 1)) \
+                or begin_index < 0 or end_index < 0:
+            raise OffsetOutOfBoundsException(
+                'One of the offsets provided is outside the length of the file.')
+        self._begin_index = begin_index
+        self._end_index = end_index
+        # Make sure that the chunk size makes sense
+        if chunk_size < 0:
+            raise InvalidChunkException(
+                'The chunk size must be greater than zero.')
+        self._chunk_size = chunk_size
+        self._resumable = resumable
+
+    def chunksize(self):
+        return self._chunk_size
+
+    def mimetype(self):
+        return self._mimetype
+
+    def size(self):
+        return self._end_index - self._begin_index
+
+    def resumable(self):
+        return self._resumable
+
+    def getbytes(self, begin: int, length: int):
+        """
+        Move to the beginning of this offset into our file
+        by calculating with the begin offset,
+        and then read the length requested.
+        Unless it goes off the end. Then shorten the length
+        to only go to to end_index
+        """
+        read_start_index: int = begin + self._begin_index
+        self._read_file.seek(read_start_index)
+        # Make sure we don't go out of bounds of our
+        # segment of the file
+        if read_start_index + length >= self._end_index:
+            length = self._end_index - read_start_index
+        return self._read_file.read(length)
+
+    def has_stream(self):
+        # We don't want it to use this interface. We want it to
+        # use getbytes
+        return False
+
+    def stream(self):
+        raise NotImplementedError(
+            "Please don't try to access the internal stream.")
+
+    def to_json(self):
+        raise NotImplementedError('This class is not serializable.')
