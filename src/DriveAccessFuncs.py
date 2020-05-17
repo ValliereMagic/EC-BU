5918292866d9d5e4d94f25f4e41c55ad76ba94a2
ValliereMagic <louisemelaney@outlook.com>
2020-05-17 19:11:58 -0400

    Added caching strategy for detecting chunk changes on upload with ECBU, only need to query once; fixed the sorting of chunk restoration in ECRS to be numeric instead of by ASCII, additional documentation.

--- src/DriveAccessFuncs.py
+++ src/DriveAccessFuncs.py
@@ -1,3 +1,7 @@
+# STL resources
+import re
+
+
 def find_or_create_backup_folder(service, dest_folder_name: str,
                                  create_mode: bool = True) -> str:
     """
@@ -38,27 +42,32 @@ def chunk_id_response_compare(item: dict):
     """
     Key function for sorting responses of
     files within a backup folder.
+
+    Remove everything from the file name up to and
+    including the '.', then convert it to an integer.
+    i.e. isolate the 23 in HHS.23
     """
-    return item.get('name')
+    return int(re.sub(r'^(.*)\.', '', item.get('name')))
 
 
-def get_chunk_file_ids(service, folder_id: str) -> list:
-    folder_ids = list()
+def get_chunk_file_information(service, folder_id: str) -> list:
+    """
+    Query google drive and acquire the ids in order of each chunk making
+    up the file to restore.
+    """
+    chunk_file_information = list()
     page_token = None
     while True:
         response = service.files().list(q="'" + folder_id + "' in parents and trashed = false",
-                                        spaces='drive', fields='nextPageToken, files(id, name)',
+                                        spaces='drive', fields='nextPageToken, files(id, name, size)',
                                         pageToken=page_token).execute()
-        # sort the response to make sure it is in order
-        # eg. HHS.1, HHS.2 ... HHS.500
-        sorted_respones: list = response.get('files', [])
-        sorted_respones.sort(key=chunk_id_response_compare)
-        # Append the ids of the chunks in sorted order.
-        for file in sorted_respones:
-            folder_ids.append(file.get('id'))
+        # Append the chunk information.
+        chunk_file_information += response.get('files', [])
         # Move on to the next page
         page_token = response.get('nextPageToken', None)
         # No more pages to look through
         if page_token is None:
             break
-    return folder_ids
+    # Sort the chunk_information so that we restore the folder in the correct order
+    chunk_file_information.sort(key=chunk_id_response_compare)
+    return chunk_file_information
